074aeea431ca613895b693015304183ed9a11f0f
Mortza81 <mortezaahmadinya.81@gmail.com>
2024-07-02 13:47:39 +0330

    adding try catch block into resolvers

--- src/data/resolvers.ts
+++ src/data/resolvers.ts
@@ -21,18 +21,28 @@ const resolvers = {
     }
   },
   perCity: async () => {
+    try{
     return await Website.aggregate([
       { $group: { _id: "$city", count: { $sum: 1 } } },
       { $project: { _id: 0, city: '$_id', count: 1 } }
     ]);
+  }
+  catch (error) {
+    console.log(error);
+  }
   },
   websitesByStars: async () => {
+    try{
       const result = await Website.aggregate([
         { $group: { _id: "$stars", count: { $sum: 1 }, websites: { $push: "$$ROOT" } } },
         { $project: { star: "$_id", count: 1, websites: 1, _id: 0 } }
       ]);
-      return result;
+      return result; 
+    }catch(error){
+      console.log(error);
+    }
   }
+  
 };
 
 export default resolvers;
